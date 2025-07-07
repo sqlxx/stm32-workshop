fe9c888b0a12569fe148062c30e24ca1c8e5b1e7
Sun Qin <sqlxx@163.com>
2025-07-07 22:43:56 +0800

    add some log

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -139,7 +139,7 @@ int main(void)
   /* USER CODE BEGIN 2 */
   LCD_Hard_Reset();
 
-
+  log_write(LOG_LEVEL_DEBUG, "System starting");
   if (HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2) != HAL_OK) {
     Blink_LED(2);
   } else {
